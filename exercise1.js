ef100334afbbdcea513b4f708cd06bac8233a63b
Gafar <104564468+iGafar@users.noreply.github.com>
2023-05-22 16:24:00 +0300

    finished exercise 1

---
+++ exercise1.js
@@ -0,0 +1,38 @@
+const parser = new DOMParser();
+
+const xmlString = `
+  <list>
+    <student>
+      <name lang="en">
+        <first>Ivan</first>
+        <second>Ivanov</second>
+      </name>
+      <age>35</age>
+      <prof>teacher</prof>
+    </student>
+    <student>
+      <name lang="ru">
+        <first>Петр</first>
+        <second>Петров</second>
+      </name>
+      <age>58</age>
+      <prof>driver</prof>
+    </student>
+  </list>
+`;
+
+const xmlDOM = parser.parseFromString(xmlString, "text/xml");
+
+const students = xmlDOM.querySelectorAll("student");
+let list = [];
+
+students.forEach((item) => {
+  list.push({
+    name: `${item.querySelector("first").textContent} ${item.querySelector("second").textContent}`,
+    age: item.querySelector("age").textContent,
+    prof: item.querySelector("prof").textContent,
+    lang: item.querySelector("name").getAttribute("lang")
+  })
+});
+
+console.log(list)
